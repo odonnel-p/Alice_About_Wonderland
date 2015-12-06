20f95c32d27eaaee23586c948aded3300a979c03
Patrick O'Donnel <patrickodonnel@Patricks-MacBook-Pro.local>
2015-12-05 20:11:01 -0500

    needs serious help

--- script/script.js
+++ script/script.js
@@ -1,18 +1,40 @@
 console.log("Literary Structure Analysis");
 
-var margin = {t:50,r:100,b:50,l:50};
+
+
+$('#search_by').submit(function(e){
+e.preventDefault();
+search_val = $( "input:first" ).val();
+
+    searchBy(search_val)
+    //console.log(e, );
+})
+
+function searchBy(search_val){
+    console.log(search_val);
+}
+
+var margin = {t:50,r:50,b:50,l:50};
 var width = document.getElementById('plot').clientWidth - margin.r - margin.l,
     height = document.getElementById('plot').clientHeight - margin.t - margin.b;
 
-var canvas = d3.select('.canvas');
-var plot = canvas
+var plot1 = d3.select('#plot')
     .append('svg')
     .attr('width',width+margin.r+margin.l)
     .attr('height',height + margin.t + margin.b)
     .append('g')
-    .attr('class','canvas')
+    .attr('class','plotty1')
     .attr('transform','translate('+margin.l+','+margin.t+')');
 
+var plot2 = d3.select('#plot2')
+    .append('svg')
+    .attr('width',width+margin.r+margin.l)
+    .attr('height',height + margin.t + margin.b)
+    .append('g')
+    .attr('class','plotty2')
+    .attr('transform','translate('+margin.l+','+margin.t+')');
+
+var scaleX = d3.scale.linear().range([0,height]);
 
 
 //Start importing data
@@ -20,27 +42,182 @@ queue()
     .defer(d3.text,'data/alice_wonderland_carroll.txt')
     .defer(d3.text,'data/looking_glass_carroll.txt')
     .await(dataLoaded)
+    
+
+function draw(wonderland, lookingGlass) {
+
+
+}
+
+
 
 
 function dataLoaded(error, wonderland, lookingGlass){
 
-//console.log(wonderland);
-//console.log(lookingGlass);
+    //choose book by button -- will program later
+    var book_select = wonderland;
+    //choose search word by submission -- will program later
+    var search_word = "Alice"
 
-//Search array (final is called nopunct)
-//use this array to search for any indidvual word, no puncuation
-var unedited_wonderland = wonderland;
-var lowerCase_wonderland = unedited_wonderland.toLowerCase();
-//console.log(lowerCase_wonderland);
-var nopunct_wonderland = lowerCase_wonderland.replace(/[^\w\s]|_/g, "")
-                                            .replace(/\s+/g, " ");
-//console.log(nopunct_wonderland);
-var a_nopunct_wonderland = nopunct_wonderland.split(' ');
-console.log(a_nopunct_wonderland);
+    //console.log(wonderland);
+    //console.log(lookingGlass);
+
+    //make an array of all words, in chronological order (no punctuation && single white spaces only)
+    var No_Punct = book_select.replace(/[^\w\s]|_/gi, "")
+                                    .replace(/\s+/gi, " ");
+    var Just_Words = No_Punct.split(' ');
+    console.log(Just_Words); //array consists of 26000+ words
 
 
-//var a_wonderland = wonderland.split(' ');
-//console.log(a_wonderland);
+    //set up for-loop to determine if search word and word in array matches
+    for( i = 0; i < Just_Words.length; i++) {
+
+        //conditional statement that draws line when match occurs
+        if(search_word == Just_Words[i]) {
+
+            //draw line
+
+        }
+    }
+
+
+
+    //-----SET UP OBJECT TO HOLD DATA
+    var this_book = {name: "Alice in Wonderland",
+                    content: book_select
+                    }
+
+    //-----BREAKDOWN INTO CHARACTERS, then build back up
+    //-----could not make this work
+    //console.log(book1.name);
+
+    //console.log(this_book.content);
+    var formatted = this_book.content.replace(/\*+/g,"").replace(/\s{2,}/g, "\n")
+    //console.log(formatted); //this returns no asterisk or extra space
+
+    var individual = formatted.split('');
+    //console.log(individual); // this returns every character in array form
+
+
+    book1 = {};
+    book1.name = this_book.name;
+    book1.content = individual;
+    book1.content.name = "chapters"
+    book1.content.content = individual.join('')
+    //console.log(book1);
+
+
+
+    //----NESTED ARRAY
+    //----Text split by (B)ook, (C)hapter, (P)aragraph, (S)entence, and (W)ord
+    //----Access: B == formatted; C == chapters
+
+
+    //-----MAKING "BOOK" LEVEL OF OBJECT
+    
+    //removed asterisk picures, then remove formatting so all paragraphs have one line break
+    var formatted = wonderland.replace(/\*+/g,"")
+                                .replace(/\s{2,}/g, "\n")
+    //console.log(formatted);
+
+    // SET UP OBJECT TO HOLD
+    book_A = {};
+    //book_A.name = "Alice in Wonderland";
+    book_A.type = "Book"
+    //book_A.author = "Lewis Carroll"
+    book_A.text = formatted; //this is a string of the whole book
+    book_A.content = {};
+    //book_A.value = formatted.length;
+
+
+
+    //-----MAKING CHAPTER LEVEL OF OBJECT
+    //split by "chapter ??? until next period"
+    var chapters = formatted.split(/chapter.+\./gi)
+                    chapters.shift();
+    //console.log(chapters);
+
+    function split_by_chapter(c) {
+        c.split(/chapter.+\./gi);
+    };
+
+    //formatted.forEach(split_by_chapter);
+
+    //console.log(formatted);
+
+    book_A.content.type = "Chapter";
+    book_A.content.text = chapters;
+    book_A.content.content = {};
+
+    //console.log(book_A);
+
+    //returns chapter 2
+    //console.log(book_A.content.text[1]);
+
+
+
+    //-----MAKING PARAGRAPH LEVEL OF OBJECT
+
+    //split each item of array
+    var paragraphs = chapters.forEach( function (p){ p.split(/\n/g) })
+    //console.log(paragraphs);
+
+    //split whole text into one "return" for each paragraph
+    //var paragraph_fix = wonderland.replace(/\↵+/gi, '↵');
+    //console.log (paragraph_fix);
+
+
+    //make array with each chapter as a separate element
+    var chapter_book = wonderland.split(/chapter/gi)
+    chapter_book.shift();
+
+    //--prints 12 chapters w/ punctuation
+    //console.log(chapter_book);
+
+
+    // var nest = d3.nest()
+    //             .key(function(b) {
+    //                 var chapters = b.split(/chapter/gi)
+    //                                 .shift();
+
+    //                 return b.chapters
+    //             })
+    //             .entries(wonderland)
+    //             .key(function(c) {
+
+    //             })
+    //             .key(function(p) {
+
+    //             })
+    //             .key(function(s) {
+
+    //             })
+
+    //console.log(nest);
+
+
+
+    //--- Nested Data attempt 2 ------------------------//
+
+
+
+
+    //button functionality
+    //switch between two datasets or books when button is clicked
+    d3.selectAll('.btn').on('click',function(){
+           var type = d3.select(this).attr('id');
+            if(type=='Wonderland'){
+                draw(wonderland);
+            }else{
+                draw(lookingGlass);
+            }
+        });
+
+}
+
+function draw(book){ }
+    //console.log(book);
+    //console.log(name);
 
     //data --> 102 element array with all item, year and value
     //creates objects with objects within grouped by date of flight
@@ -145,7 +322,8 @@ console.log(a_nopunct_wonderland);
         .enter()
         .append('path') //creates two new <path> elements as the enter set
         .attr('class', function(item){return item.key}) //each element will have class of either "coffee" or "tea"*/
-}
+
+
 
 // function attachTooltip(selection){
 //     selection
